d98fbc689bcf444bfa9a85e524cdb821dc51aec4
Eric Phillips <ericj.phillips@gmail.com>
2016-07-10 20:58:35 -0400

    feat: grab owned game data for all group members
    also writes that data to a file
    which is good because these requests
    take forever

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
+require('dotenv').config()
 const http = require('http')
 const xml2js = require('xml2js')
-
+const fetch = require('node-fetch')
+const fs = require('fs')
 var parser = new xml2js.Parser()
 
 var groupUrl = 'http://steamcommunity.com/groups/redditpd/memberslistxml/?xml=1'
-var groupMembers = []
+var steamUserUrl = function (userId) {
+  return `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${process.env.API_KEY}&steamid=${userId}&format=json&include_appinfo=1`
+}
 
-function getGroupMembersList (url) {
+let memberList = []
+
+var getGroupMembersList = function (url) {
   let request = http.get(url, (response) => {
     let xml = ''
-
     response.on('data', (chunk) => {
       xml += chunk
     })
 
     response.on('end', () => {
       parser.parseString(xml, (error, result) => {
-        let steamIdArray = result.memberList.members[0].steamID64
-        console.dir(steamIdArray)
-        Array.prototype.push.apply(groupMembers, steamIdArray)
-        if (result.memberList.nextPageLink) {
-          getGroupMembersList(result.memberList.nextPageLink[0])
+        if (error) {
+          return error
         } else {
-          console.log(groupMembers.length)
-          console.log('done!')
-          return
+          let steamIdArray = result.memberList.members[0].steamID64
+          Array.prototype.push.apply(memberList, steamIdArray)
+          if (result.memberList.nextPageLink) {
+            getGroupMembersList(result.memberList.nextPageLink[0])
+          } else {
+            console.log('done!')
+            Promise.resolve(memberList)
+            .then((ids) => {
+              return Promise.all(ids.map((id) => {
+                return fetch(steamUserUrl(id))
+                .then((text) => { return text.json })
+                .then((json) => { return json.response })
+              }))
+            })
+            .then((data) => {
+              fs.writeFile('./membersList.json', data)
+            })
+          }
         }
       })
     })
