6ddf503d21a0f1691cf782ebdcaf91fca2812519
Eric Phillips <ericj.phillips@gmail.com>
2016-12-12 08:44:54 -0500

    Move XML get into a nice fetch statement

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
 require('dotenv').config()
-const http = require('http')
-const xml2js = require('xml2js')
+const mongoose = require('mongoose')
 const fetch = require('node-fetch')
-const fs = require('fs')
-var parser = new xml2js.Parser()
+const xml2js = require('xml2js').parseString
+const xmlUri = process.env.XML_PATH
 
-var groupUrl = 'http://steamcommunity.com/groups/redditpd/memberslistxml/?xml=1'
-var getGamesUrl = function (userId) {
-  return `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${process.env.API_KEY}&steamid=${userId}&format=json&include_appinfo=1&include_played_free_games=1`
+mongoose.connect(process.env.DB_URI)
+let Member = mongoose.model('Member', { steamId: String })
+
+function userGamesPath (steamId) {
+  return 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/' +
+  `?key=${process.env.API_KEY}&steamid=${steamId}` +
+  '&format=json&include_appinfo=1&include_played_free_games=1'
 }
 
-let memberList = []
-
-var getGroupMembersList = function (url) {
-  let request = http.get(url, (response) => {
-    let xml = ''
-    response.on('data', (chunk) => {
-      xml += chunk
-    })
-
-    response.on('end', () => {
-      parser.parseString(xml, (error, result) => {
-        if (error) {
-          return error
-        } else {
-          let steamIdArray = result.memberList.members[0].steamID64
-          Array.prototype.push.apply(memberList, steamIdArray)
-          if (result.memberList.nextPageLink) {
-            getGroupMembersList(result.memberList.nextPageLink[0])
-          } else {
-            console.log('done!')
-            Promise.resolve(memberList)
-            .then((ids) => {
-              return Promise.all(ids.map((id) => {
-                return fetch(getGamesUrl(id))
-                .then((text) => { return text.json() })
-                .then((json) => { return json.response })
-              }))
-            })
-            .then((data) => {
-              fs.writeFile('membersList.json', JSON.stringify(data))
-            })
-          }
-        }
-      })
+fetch(xmlUri)
+  .then(res => { return res.text() })
+  .then(xml => {
+    xml2js(xml, function (err, result) {
+      if (err) {
+        console.log(err)
+        return false
+      } else {
+        console.dir(result)
+        return true
+      }
     })
   })
-}
-
-getGroupMembersList(groupUrl)
